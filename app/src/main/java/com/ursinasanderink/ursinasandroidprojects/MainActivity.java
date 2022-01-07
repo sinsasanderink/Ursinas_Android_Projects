2f814a6826c3d1f6ec5e7f6fb667455b1329fc95
sinasanderink <sanderink.ursina@yahoo.com>
2022-01-07 11:00:57 -0600

    .

--- app/src/main/java/com/ursinasanderink/ursinasandroidprojects/MainActivity.java
+++ app/src/main/java/com/ursinasanderink/ursinasandroidprojects/MainActivity.java
@@ -1,8 +1,11 @@
 package com.ursinasanderink.ursinasandroidprojects;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,15 +14,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        RecyclerView list = findViewById(R.id.recycler_view_projects);
+
         Project[] projects = {
                 new Project("Getting Started App", "Our very first project, the default 'Hello World' app!", R.drawable.getting_started),
                 new Project("My Quote App", "Making a simple change to the layout, with my own personal motivational quote for software developers", R.drawable.quote),
                 new Project("BMI Calculator", "A real life working BMI calculator teaching me Variables, Methods and Conditional Logic", R.drawable.calculator),
                 new Project("Inches Converter", "A basic converter to convert inches to meters (my own personal calculator app made on my own)", R.drawable.tape),
                 new Project("The Hungry Developer", "A menu app for a fictional restaurant, learning about Activities, Classes & Objects, Arrays, Intents and ListViews", R.drawable.hungry_developer)
-
         };
 
+        ProjectsAdapter adapter = new ProjectsAdapter(projects);
+
+        list.setAdapter(adapter);
 
     }
 }
